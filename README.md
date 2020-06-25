2c5ce0fe155b06916a72ba22902f1cd3f199c114
José Corrêa <joscorvi@gmail.com>
2020-06-25 13:49:05 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# A quick example of Structured Logs 
+
+## Important!
+
+You need have a Seq running in your machine. [To run as a container, click here to configure Seq with Docker](https://hub.docker.com/r/datalust/seq/) (link to Datalust Seq Hub).
+
+Enjoy! :)
---
+++ src/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Serilog;
+
+namespace SerializedLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ILogger log = new LoggerConfiguration()
+                .WriteTo.Seq("http://localhost:5341")
+                .CreateLogger(); 
+
+                for (int i = 0; i < 1500; i++)
+                {
+                    var request = new 
+                    {
+                        TransactionId = Guid.NewGuid(),
+                        Message = "OK",
+                        Response = "Usuário salvo com sucesso!"
+                    };
+
+                    log.Information("New request transactionId: {TransactionID}: {@request}", request.TransactionId, request);
+                    
+                }
+
+                Console.Read();
+        }
+    }
+}
